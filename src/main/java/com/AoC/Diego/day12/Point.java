ccc95216904ecb021e9c66294c16993a453b4a9c
DiegoCedresE <91256834+DiegoCedresE@users.noreply.github.com>
2026-06-30 14:21:12 +0100

    Actualizar Point.java

--- src/main/java/com/AoC/Diego/day12/Point.java
+++ src/main/java/com/AoC/Diego/day12/Point.java
@@ -1,3 +1,8 @@
 package com.AoC.Diego.day12;
-
+/**
+ * Representa un punto mediante sus coordenadas X e Y.
+ *
+ * @param x coordenada horizontal.
+ * @param y coordenada vertical.
+ */
 public record Point(int x, int y) {}
