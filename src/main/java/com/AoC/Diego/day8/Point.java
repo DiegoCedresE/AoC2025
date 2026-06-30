d1d58b644eefe12692991c2c401ab869170c7d36
DiegoCedresE <91256834+DiegoCedresE@users.noreply.github.com>
2026-06-30 14:07:23 +0100

    Actualizar Point.java

--- src/main/java/com/AoC/Diego/day8/Point.java
+++ src/main/java/com/AoC/Diego/day8/Point.java
@@ -1,27 +1,34 @@
 package com.AoC.Diego.day8;
-
+/**
+ * Representa un punto en un espacio tridimensional.
+ */
 public class Point {
-
     private final int x;
     private final int y;
     private final int z;
-
     public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-
+    /**
+     * Devuelve la coordenada X del punto.
+     *
+     * @return coordenada X.
+     */
     public int getX() {
         return x;
     }
-
+    /**
+     * Calcula la distancia euclídea entre este punto y otro.
+     *
+     * @param other punto con el que se calcula la distancia.
+     * @return distancia entre ambos puntos.
+     */
     public double distance(Point other) {
-
         long dx = x - other.x;
         long dy = y - other.y;
         long dz = z - other.z;
-
         return Math.sqrt(dx * dx + dy * dy + dz * dz);
     }
 }
